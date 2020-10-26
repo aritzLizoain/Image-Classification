934ee8707e6c0b3c3dbdf6c0a42bb97bd4811146
aritzLizoain <65019180+aritzLizoain@users.noreply.github.com>
2020-10-26 17:55:49 +0100

    Uploading the codes, images and data

---
+++ Code/image_recognition.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 19:01:39 2020
+
+@author: Aritz Lizoain
+
+Followed: https://medium.com/intuitive-deep-learning/build-your-first-convolutional-neural-network-to-recognize-images-84b9c78fe0ce
+
+Image recognition with Keras (CIFAR-10 standard dataset)
+1) Data processing: one-hot encoding and scaling
+2) Building and training the CNN
+3) Training the model
+4) Model training process evaluation
+5) Evaluation of the model
+6) Saving the trained model
+"""
+
+from keras.datasets import cifar10 # CIFAR-10 dataset
+import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential #to set the CNN architecture
+from keras.layers import Dense, Dropout, Flatten, Conv2D,\
+    MaxPooling2D # NN layers
+import pickle #to save the datasets
+import random
+
+"""
+1) Data processing
+*One-hot encoding labels keras.utils.to_categorical
+*Scale image pixel values: change data type and divide by 255
+"""
+
+#Loading the dataset: CIFAR-10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+#50000 training and 10000 testing samples.
+#Training images: 32 pixels in height, 32 pixels in width, 3 pixels in depth
+#Labels: 1 number (corresponding to the category) for each image. 
+
+
+
+#Visualize a random image
+random = random.randint(0, len(x_train))
+img=plt.imshow(x_train[random])
+print('The label of this category is: ',y_train[random])
+#0 airplane, 1 automobile, 2 bird, 3 cat, 4 deer, 5 dog, 6 frog,
+#7 horse, 8 ship, 9 truck
+
+#One-hot encoding conversion with Keras
+y_train_one_hot=keras.utils.to_categorical(y_train,10)
+y_test_one_hot=keras.utils.to_categorical(y_test,10)
+print('the one hot label of the random image is:', y_train_one_hot[random])
+
+#Pixel values take value between 0 and 255 (RGB scale) -> make them between 0 and 1
+x_train=x_train.astype('float32') #convert the type to float32, which is 
+#a datatype that can store values with decimal points. then divide by 255
+x_test=x_test.astype('float32')
+x_train=x_train/255
+x_test=x_test/255
+
+#I will save x_test and y_test in order to test the model the other file (my_image_recognition.py)
+pickle.dump(x_test, open("Data/x_test.dat", "wb"))
+pickle.dump(y_test_one_hot, open("Data/y_test.dat", "wb")) 
+#BE CAREFUL WITH PATH AND OVERWRITING DATA
+
+"""
+2) Building and training the CNN
+*Defining the CNN architecture with Keras Sequential model
+*Compiling the model
+"""
+
+#ARCHITECTURE: (ConvX2, Max Pool, Dropout)X2, FC, Dropout, Fc, Softmax
+
+model=Sequential() #create empty sequential model and then add layers
+
+#Layer 1: conv layer, filter size 3X3, stride size 1 in both dimensions,
+#depth 32. Padding same and activation relu will apply to all layers.
+#we will use ReLU activation for all our layers, except for the last layer
+#remember that ReLU doesn't map negative values (no negative values here)
+#no specification of stride default setting = 1
+#input shape needs to be specified, but not for the following layers
+model.add(Conv2D(32,(3,3), activation='relu', padding='same',\
+                 input_shape=(32,32,3)))
+
+#Layer 2: conv layer, filter size 3X3, stride size 1 in both dimensions,
+#depth 32. Padding same and activation relu will apply to all layers.
+#we would need padding 1 to achieve the same width and height, but 
+#we will use 'same' padding for all the conv layers, aka zero pad.
+model.add(Conv2D(32,(3,3), activation='relu', padding='same'))
+    
+#Layer 3: max pooling layer, pool size 2X2, stride 2 in both dimensions,
+#max pooling layer stride default given by pool size
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+#Layer 4: dropout layer with probability 25% of dropout, to prevent overfitting
+model.add(Dropout(0.25))
+
+#Layers 5-8: same but depth of conv layer is 64 instead of 32
+model.add(Conv2D(64,(3,3), activation='relu', padding='same'))
+model.add(Conv2D(64,(3,3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+#Layer 9: FC (Fully-Connected) layer. Now our neurons are not just in 
+#on row, but spatially arranged in a cube-like format. We need to make
+#them into one row, flattening. Flatten layer.
+model.add(Flatten()) #now one row
+model.add(Dense(512,activation='relu')) #dense (FC) layer. 
+
+#Layer 10: another dropout of probability 50%
+model.add(Dropout(0.5))
+
+#Layers 11-12: another dense (FC) layer with 10 neurons and sofrmax activation
+#last layer, softmax, only transforms the output of the previous layer 
+#into probability distributions, which is the final goal
+model.add(Dense(10,activation='softmax'))
+
+#end of architecture
+model.summary()
+
+#COMPILING THE MODEL: model.compile
+#First we need to specify with algorithm to use for the optimization, what loss 
+#function to use and what other metrics to track apart from the loss function
+#optimizer: adam. Adds some tweaks to stcochastic gradient 
+#descent such that it reaches the lower loss function faster 
+#loss: categorical crossentropy = loss function for classification.
+#metrics: accuracy = we want to track accuracy on top of the loss function
+model.compile(loss='categorical_crossentropy', optimizer='adam',\
+              metrics=['accuracy'])
+
+"""
+3) Training the model
+(highly time-consuming)
+"""
+
+#We are fitting the parameters to the data. We specify the data we are 
+#training on, then the size of our mini-batch and how long we want to train it
+#for. Last, specify the validation data, that will tell us how we are doing on
+#the validation data at each point. We didn't split it before, we now specify
+#how much of our dataset will be used as a validation set. In this case, 20% will be validation set.
+hist=model.fit(x_train,y_train_one_hot, batch_size=32,epochs=20,\
+               validation_split=0.2)
+    
+    
+"""
+4) Model training process evaluation
+If the improvements in our model to the training set look matched up with the 
+imporvements to the validation set, it doesn't seem like overfitting is a huge
+problem in this model.
+"""
+
+# LOSS 
+plt.plot(hist.history['loss']) #variable 1 to plot
+plt.plot(hist.history['val_loss']) #variable 2 to plot
+plt.title('Model loss') #title
+plt.ylabel('Loss') #label y
+plt.xlabel('Epoch') #label x
+plt.legend(['Training', 'Validation'], loc='upper right') #legend
+plt.show() #display the graph
+
+# ACCURACY
+plt.plot(hist.history['accuracy']) #variable 1 to plot
+plt.plot(hist.history['val_accuracy']) #variable 2 to plot
+plt.title('Model accuracy') #title
+plt.ylabel('Accuracy') #label y
+plt.xlabel('Epoch') #label x
+plt.legend(['Training', 'Validation'], loc='lower right') #legend
+plt.show() #display the graph
+
+"""
+5) Evaluation of the model (on the test set)
+"""
+
+print('The accuracy of the model on the test set is: ',\
+      model.evaluate(x_test,y_test_one_hot)[1]*100,'%')
+# The accuracy of the model on the test set is: 77.25%
+
+"""
+6) Saving the trained model
+The model will be saved in a file format called HDF5
+In order to load it run: 
+    from keras.models import load_model
+    model = load_model('my_cifar10_model.h5')
+"""
+
+model.save('model_name.h5') #be careful, don't overwrite
---
+++ Code/my_image_recognition.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 17:25:36 2020
+
+@author: Aritz Lizoain
+
+Followed: https://medium.com/intuitive-deep-learning/build-your-first-convolutional-neural-network-to-recognize-images-84b9c78fe0ce
+
+Two trained models:
+    *my_cifar10_model : original model. 77.25% accuracy on the test set.
+    *my_cifar10_model2_augmented : model trained after applying data augmentation.
+     78.04% accuracy on the test set.
+
+1) Loading a trained model
+2) Predicting on the test set
+3) Evaluation of the predictions
+4) Predicting on OWN IMAGES
+
+"""
+
+from keras.models import load_model #loading the model
+from skimage.transform import resize #resize image
+import numpy as np 
+import matplotlib.pyplot as plt
+import pickle #data loading
+from sklearn.metrics import classification_report #classification report
+import random
+
+"""
+1) Loading a trained model
+"""
+
+#model = load_model('Models/my_cifar10_model.h5') # original model
+model = load_model('Models/my_cifar10_model2_augmented.h5') # +data augmentation model
+
+"""
+2) Predicting on the test set
+"""
+
+#Load the test set
+x_test=pickle.load(open("Data/x_test.dat","rb"))
+y_test=pickle.load(open("Data/y_test.dat","rb"))
+
+y_test_label=np.argmax(np.round(y_test),axis=1)
+
+number_to_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog',\
+                   'frog', 'horse', 'ship', 'truck']
+
+#Predict on the test set
+predicted_classes=model.predict(x_test)   
+#finding the positon (=label) of the prediction and solution
+predicted_classes_label=np.argmax(np.round(predicted_classes),axis=1)
+
+"""
+3) Evaluation of the predictions
+"""
+
+#Comparing correct answers
+correct=np.where(predicted_classes_label==y_test_label)[0] #need to have the same shape
+print("Found", len(correct), "correct classes")   
+#Comparing incorrect answers
+incorrect=np.where(predicted_classes_label!=y_test_label)[0] #need shape
+print("Found", len(incorrect), "incorrect classes")
+#Visusalizing a random incorrect prediction
+random = random.randint(0, len(incorrect))
+plt.subplot(2,2,1)
+plt.imshow(x_test[incorrect[random]].reshape(32,32,3),cmap='gray',interpolation='none')
+plt.title('Predicted '+str(number_to_class[predicted_classes_label[incorrect[random]]])+\
+          ', correct '+str(number_to_class[y_test_label[incorrect[random]]]))
+plt.tight_layout()
+
+
+#Classification report sklearn.metrics.classification_report
+#Will help identifiying the misclassified classes in more detail. 
+#Able to observe for which class the model performed better or worse.
+target_names = [number_to_class[i] for i in range(10)]
+print(classification_report(y_test_label, predicted_classes_label,\
+                            target_names=target_names))              
+#Recal:"how many of this class you find over the whole number of element of
+# this class"
+#Precision:"how many are correctly classified among that class"
+#F1-score:the harmonic mean between precision & recall. Good on inbalanced sets, like this one
+#Support:the number of occurence of the given class in your dataset
+
+"""
+4) Predicting on OWN IMAGES
+*Prepare the image to recognize
+*Predict on the image
+*Analyze the prediction
+"""
+
+# PREPARE THE IMAGE TO RECOGNIZE
+#reading the file as an array of pixel values
+#the image is reshaped to (32,32,3)
+#if the image is originally not squared, shape will be lost and recognition will be more likely to fail.
+my_image=plt.imread("Images/my_image_1.jpg")
+#resize image to fit model
+#model image size: 32*32*3
+my_image_resized=resize(my_image, (32,32,3)) #already makes values between 0-1
+#visualize the image
+img=plt.imshow(my_image) #will only show one
+#img_resized=plt.imshow(my_image_resized)
+
+# PREDICT ON THE IMAGE
+probabilities=model.predict(np.array([my_image_resized]))
+#np.array changes the current array of the pixel values into a 4D array
+#because model.predict expects a 4D array (3D+training examples). 
+#Training set and test set were consistent with this before
+#10 output neurons corresponding to a probability distribution over the classes
+
+# ANALYZE THE PREDICTION
+index = np.argsort(probabilities[0,:])
+print("Most likely class:", number_to_class[index[9]], "-- Probability:",\
+      probabilities[0,index[9]])
+print("Second most likely class:", number_to_class[index[8]], "-- Probability:",\
+      probabilities[0,index[8]])
+print("Third most likely class:", number_to_class[index[7]], "-- Probability:",\
+      probabilities[0,index[7]])
+print("Fourth most likely class:", number_to_class[index[6]], "-- Probability:",\
+      probabilities[0,index[6]])
+print("Fifth most likely class:", number_to_class[index[5]], "-- Probability:",\
+      probabilities[0,index[5]])
